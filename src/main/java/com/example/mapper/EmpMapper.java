855bd41b0d7dee6c529c05f5974463e33f5380ee
tangxujia <2913223934@qq.com>
2026-08-03 15:07:59 +0800

    feat: 完善分页查询

--- src/main/java/com/example/mapper/EmpMapper.java
+++ src/main/java/com/example/mapper/EmpMapper.java
@@ -3,7 +3,6 @@ package com.example.mapper;
 
 import com.example.pojo.Emp;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -14,18 +13,25 @@ import java.util.List;
  */
 @Mapper
 public interface EmpMapper {
-  /**
-   * 统计总记录数
-   * @return
-   */
-  @Select("SELECT count(*) FROM emp e left join dept d on e.dept_id = d.id")
-  public Long count();
+  // ---------------------------------------原始分页查询方式------------------------------------------
+  // /**
+  //  * 统计总记录数
+  //  * @return
+  //  */
+  // @Select("SELECT count(*) FROM emp e left join dept d on e.dept_id = d.id")
+  // public Long count();
+  //
+  // /**
+  //  * 分页查询
+  //  * @return
+  //  */
+  // @Select("SELECT e.*, d.name deptName FROM emp e left join dept d on e.dept_id = d.id " +
+  //     "order by e.update_time desc limit #{start},#{pagesize};")
+  // public List<Emp> list(@Param("start") Integer start, @Param("pagesize") Integer pagesize);
 
-  /**
-   * 分页查询
-   * @return
-   */
+  // =-----------------------------------采用pageHelper分页查询------------------------------------
+  // 注意：sql语句不能加分号、
   @Select("SELECT e.*, d.name deptName FROM emp e left join dept d on e.dept_id = d.id " +
-      "order by e.update_time desc limit #{start},#{pagesize};")
-  public List<Emp> list(@Param("start") Integer start, @Param("pagesize") Integer pagesize);
+      "order by e.update_time desc")
+  public List<Emp> list();
 }
--- src/main/java/com/example/service/impl/EmpServiceImpl.java
+++ src/main/java/com/example/service/impl/EmpServiceImpl.java
@@ -4,6 +4,8 @@ import com.example.mapper.EmpMapper;
 import com.example.pojo.Emp;
 import com.example.pojo.PageResult;
 import com.example.service.EmpService;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,14 +16,31 @@ public class EmpServiceImpl implements EmpService {
   @Autowired
   private EmpMapper empMapper;
 
-  @Override
+  // @Override
+  // public PageResult<Emp> findByPage(Integer page, Integer pagesize) {
+  //   // 引入pageHelper
+  //
+  //   // 1、调用mapper接口，查询总记录数
+  //   Long total = empMapper.count();
+  //   // 2、调用mapper接口，查询分页数据
+  //   Integer start = (page - 1) * pagesize;
+  //   List<Emp> rows = empMapper.list();
+  //   // 3、封装PageResult对象，返回
+  //   return new PageResult<Emp>(total, rows);
+  // }
+
+
+  // 基于pageHelper查询
+  // @Override
   public PageResult<Emp> findByPage(Integer page, Integer pagesize) {
-    // 1、调用mapper接口，查询总记录数
-    Long total = empMapper.count();
-    // 2、调用mapper接口，查询分页数据
-    Integer start = (page - 1) * pagesize;
-    List<Emp> rows = empMapper.list(start, pagesize);
-    // 3、封装PageResult对象，返回
-    return new PageResult<Emp>(total, rows);
+    // 设置pageHelper分页参数
+    PageHelper.startPage(page, pagesize);
+
+    // 执行查询
+    List<Emp> empList = empMapper.list();
+
+    // 解析查询结果，封装PageResult对象，返回
+    Page<Emp> p = (Page<Emp>) empList;
+    return new PageResult<Emp>(p.getTotal(),p.getResult());
   }
 }
